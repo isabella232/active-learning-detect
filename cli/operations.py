32021b4e0e962042e1e9726f1e95dc419481d505
Bhargav Nookala <nooknb@gmail.com>
2019-01-02 16:29:25 -0800

    Uploading with full paths (#107)

--- cli/operations.py
+++ cli/operations.py
@@ -17,11 +17,15 @@ UPPER_LIMIT = 100
 
 azure_storage_client = None
 
+
 class ImageLimitException(Exception):
     pass
 
 
 def supported_file_type(file_name):
+    if file_name.startswith('.'):
+        return False
+
     file_suffix = pathlib.Path(file_name).suffix.lower()
     if file_suffix in ['.png', '.jpg', '.jpeg', '.gif']:
         return True
@@ -29,50 +33,72 @@ def supported_file_type(file_name):
         return False
 
 
+# Somewhat of a hack to remove the folder name passed on the
+# command line from the file path to make blob names more sane.
+# If one invokes the cli with the onboard folder option and passes
+# along a path to a folder, every file path will contain the folder prefix.
+# Example invocation: python3 -m cli onboard /my/full/path
+# os.walk will return: /my/full/path/1.jpg, /my/full/path/2.jpg, etc.
+def strip_path_prefix(folder_name, file_path):
+    folder_name_str = str(folder_name)
+    file_path_str = str(file_path)
+
+    stripped_path = file_path_str.replace(folder_name_str, "")
+
+    if stripped_path.startswith('/'):
+        return pathlib.Path(stripped_path[1:])
+
+    return pathlib.Path(stripped_path)
+
+
 # TODO We should create the container if it does not exist
 def onboard_folder(config, folder_name):
     blob_storage = BlobStorage.get_azure_storage_client(config)
-    uri = 'https://' + config.get("storage_account") + '.blob.core.windows.net/' + config.get("storage_temp_container") + '/'
-    functions_url = config.get('url') + '/api/onboarding'
     user_name = config.get("tagging_user")
-    images = []
+    onboarding_files = []
 
-    for image in os.listdir(folder_name):
-        if not supported_file_type(image):
+    print("Walking file system")
+
+    for (root, dirs, files) in os.walk(folder_name):
+        # no files at this level.
+        if len(files) == 0:
             continue
 
-        local_path = os.path.join(folder_name, image)
-        print('Uploading image ' + image)
+        for file_name in files:
+            if not supported_file_type(file_name):
+                continue
 
-        # Upload the created file, use image name for the blob name
+            relative_path = os.path.join(root, file_name)
+            onboarding_files.append(relative_path)
+
+    if len(onboarding_files) == 0:
+        print(f'Could not find any valid files to upload')
+        return
+
+    for blob_path in onboarding_files:
+        stripped_path = strip_path_prefix(folder_name, blob_path)
+        print("Uploading " + str(blob_path))
+
+        blob_metadata = {
+            "userFilePath": blob_path,
+            "uploadUser": config.get("tagging_user")
+        }
+
         blob_storage.create_blob_from_path(
             config.get("storage_temp_container"),
-            image,
-            local_path,
-            content_settings=ContentSettings(content_type='image/png')
+            stripped_path,  # the name of the file in blob storage.
+            pathlib.Path(blob_path),
+            content_settings=ContentSettings(content_type='image/png'),
+            metadata=blob_metadata
         )
 
-        images.append(uri + image)
-
-    # Post this data to the server to add them to database and kick off active learning
-    data = {}
-    data['imageUrls'] = images
-
-    query = {
-        "code": config.get('key'),
-        "userName": user_name
-    }
-
-    response = requests.post(functions_url, json=data, params=query)
-    response_json = response.json()
-    response.raise_for_status()
-
-    if 'Success' in response_json:
-        print("Successfully onboarded {0} images.".format(len(images)))
-    if 'copy_failed' in response_json:
-        print("Failed to copy following images to permanent storage: " + str(response_json['copy_failed']))
-    if 'delete_failed' in response_json:
-        print("Failed to delete following images from permanent storage: " + str(response_json['delete_failed']))
+    # Trigger queue based onboarding.
+    onboard_container(
+        config,
+        config.get('storage_account'),
+        config.get('storage_key'),
+        config.get('storage_container')
+    )
 
 
 def onboard_container(config, account, key, container):
@@ -129,7 +155,7 @@ def download(config, num_images, strategy=None):
     count = len(images_json)
 
     print("Received " + str(count) + " files.")
-    
+
     if count == 0:
         print("No images could be retrieved with the current retrieval strategy!")
         return
@@ -173,7 +199,7 @@ def download_images(config, image_dir, json_resp):
     for url_path in urls:
         url = URL(url_path)
 
-        #   TODO: We will download an empty file if we get a permission error on the blob store URL
+        # TODO: We will download an empty file if we get a permission error on the blob store URL
         # We should raise an exception. For now the blob store must be publically accessible
         response = requests.get(url)
         file_path = pathlib.Path(image_dir / url.name)
@@ -187,7 +213,7 @@ def download_images(config, image_dir, json_resp):
 
 
 def write_vott_data(image_dir, json_resp):
-    #VOTT expects json file at same level as directory
+    # VOTT expects json file at same level as directory
     data_file = pathlib.Path(image_dir / "../data.json")
     vott_data = json_resp.get("vott_json", None)
 
